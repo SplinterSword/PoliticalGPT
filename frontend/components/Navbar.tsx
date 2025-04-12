c3e02ad8cafd31319bba66ab06ddc62aba1d791d
SplinterSword <sparshj2003@gmail.com>
2025-04-12 19:08:06 +0530

    Fininshing Up

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
       <nav className="flex items-center justify-between p-4 md:p-6">
         <Link href="/" className="text-2xl font-bold tracking-tight">
-          PolCon
+          PoliticalGPT
         </Link>
         <Link 
           href="https://github.com/SplinterSword/PoliticalGPT" 
